f5752e89a12816639e8c1db373fd165d8626c7ba
Vlad Kamyshov <flurescein@gmail.com>
2020-07-07 15:40:07 +0300

    Add port from env.

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,8 @@ import cors from 'cors'
 import { Resolvers } from './resolvers'
 
 async function main() {
+  const port = process.env.PORT || 1488
+
   await createConnection()
 
   const schema = await buildSchema({ resolvers: [Resolvers] })
@@ -22,9 +24,9 @@ async function main() {
   app.use(helmet())
   app.use(cors())
 
-  app.listen(4000, () =>
+  app.listen(port, () =>
     console.log(
-      `GraphQL server started on http://localhost:4000${apolloServer.graphqlPath}`
+      `GraphQL server started on http://localhost:${port}${apolloServer.graphqlPath}`
     )
   )
 }
